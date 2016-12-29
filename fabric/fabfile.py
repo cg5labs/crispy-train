afdf6fada9d5623e19320026ed40c4a1f09297f9
Cloud User <ec2-user@ip-172-31-41-84.us-west-2.compute.internal>
2016-12-29 07:58:43 -0500

    Added fabric examples for multi-host deployment.

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -1,4 +1,38 @@
-from fabric.api import run
+from fabric.api import run, env, roles, execute
+from fabric.colors import red, green
+
+#env.hosts = [ 'localhost', '127.0.0.1' ]
+
+env.roledefs = {
+    'db': ['localhost'],
+    'web': ['127.0.0.1'],
+}
 
 def host_type():
     run('uname -a')
+
+def diskfree():
+    run('df -h')
+
+def color_db():
+  print(green("DB deployment GREEN") + red(" and some red characters...") + ".")
+
+def color_web():
+  print(red("WEB deployment in RED."))
+
+# role-specific tasks
+@roles('db')
+def migrate():
+    # Database stuff here.
+    color_db()
+
+@roles('web')
+def update():
+    # Code updates here.
+    color_web()
+
+# multi-host deployment script
+
+def deploy():
+    execute(migrate)
+    execute(update)
