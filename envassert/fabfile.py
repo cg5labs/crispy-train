3323a3c695dc68b5cb49d92edb8e09feeb2475a4
Cloud User <ec2-user@ip-172-31-41-84.us-west-2.compute.internal>
2016-12-31 04:53:16 -0500

    Added envassert. Serverspec-like python test suite.

---
+++ envassert/fabfile.py
@@ -0,0 +1,40 @@
+
+from fabric.api import env, task
+from envassert import file, process, package, user, group, port, cron, detect, service, filesystem
+
+env.use_ssh_config = True
+
+@task
+def check():
+    env.platform_family = detect.detect()
+
+    assert file.exists("/etc/hosts")
+    assert file.is_file("/etc/hosts")
+    assert file.is_dir("/tmp/")
+    assert file.dir_exists("/tmp/")
+    assert file.has_line("/etc/passwd", "sshd")
+    assert file.owner_is("/bin/sh", "root")
+    assert file.group_is("/bin/sh", "root")
+    assert file.mode_is("/bin/sh", "777")
+
+    if env.platform_family == "freebsd":
+        assert file.is_link("/compat")
+    else:
+        assert file.is_link("/usr/tmp")
+
+    # this check needs to run as root
+    #assert package.installed("wget.x86_64")
+
+    assert user.exists("sshd")
+    # this check doesn't work with systemd
+    #assert service.is_enabled("sshd")
+    assert user.is_belonging_group("ec2-user", "adm")
+    # module error on group.exist
+    #assert group.exists("adm")
+
+    assert port.is_listening(22)
+    #assert port.is_listening(80, "tcp")
+
+    assert process.is_up("http") is False
+
+    assert filesystem.is_type('xfs', '/')
